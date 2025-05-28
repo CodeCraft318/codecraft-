5f8ba39b927ad16f9617a5d7521e00019cdac94b
raj chauhan <rajgitgithub.com>
2025-05-28 22:59:51 +0530

    -hosting

--- vite.config.js
+++ vite.config.js
@@ -15,6 +15,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/codecraft-/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
